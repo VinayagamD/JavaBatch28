47cf337db77a1de088d6c156c95ecf24192dfb30
VinayagamD <vinayagam.d.ganesh@gmail.com>
2022-06-30 21:41:39 +0530

    Intial Code for java 8

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Course");
+    }
+}
+
+class Test1 {}
+class Test2 {}
+class Test3 {}
+class Test4 {}
+
